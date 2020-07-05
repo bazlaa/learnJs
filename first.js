6be4f44c1f7a987a56116268ed04744954cfa7a5
VIKTAR <kasilkina@mail.ru>
2020-07-05 14:10:12 +0300

    first

---
+++ first.js
@@ -0,0 +1,167 @@
+ 'use strict';
+
+ // let persone = {
+ //     name: 'John',
+ //     age: 25,
+ //     isMarried: false
+ // };
+
+ // console.log(persone['name']);
+
+
+ // let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+ // console.log(arr[0]);
+
+
+
+ // // let answer = confirm('hello World');
+ // // console.log(answer);
+
+ // // let answer = +prompt("Есть ли вам 18?");
+ // // console.log(typeof(answer));
+
+
+ // // console.log("arr" + "- object");
+ // // console.log(4 + +"- object");
+
+ // let incr = 10,
+ //     decr = 10;
+
+ // incr--;
+ // --decr;
+
+ // console.log(incr);
+ // console.log(decr);
+
+ // console.log(5 % 2);
+ // console.log("2" == 2); //сравнивание
+ // console.log("2" === 2); // строгое сравнивание по типу данных
+
+ // // let isChecked = true,
+ // //     isClose = false;
+ // // console.log(isChecked && isClose); // оператор И
+
+
+ // // let isChecked = true,
+ // //     isClose = false;
+ // // console.log(isChecked || isClose); // оператор Или
+
+ // let a = 5;
+ // console.log(a);
+
+
+ //  let num = 50;
+
+ //  if (num < 49) {
+ //      console.log("Неверно");
+ //  } else if (num > 100) {
+ //      console.log("Многовато");
+ //  } else {
+ //      console.log("Верно");
+ //  };
+
+ //  (num == 50) ? console.log("Верно"): console.log("Неверно");
+
+ //  switch (num) {
+ //      case num < 49:
+ //          console.log("Неверно");
+ //          break;
+ //      case num > 100:
+ //          console.log("Многовато");
+ //          break;
+ //      case num > 80:
+ //          console.log(" Всё ещё многовато");
+ //          break;
+ //      case 50:
+ //          console.log("Верно");
+ //          break;
+ //      default:
+ //          console.log("Что то пошло не так");
+ //          break;
+ //  }
+ //let num = 50;
+ //  while (num < 55) {
+ //      console.log(num);
+ //      num++;
+ //  }
+
+ //  do {
+ //      console.log(num);
+ //      num++;
+ //  }
+ //  while (num < 55);
+
+ //  for (let i = 1; i < 8; i++) {
+ //      if (i == 6) {
+ //          continue;
+ //      }
+ //      console.log(i);
+ //  }
+
+ //const obj = {
+ // a: 50
+ //};
+
+ // obj.a = 10;
+ //console.log(obj);
+
+
+ //  let MyName = "Victor";
+ //  console.log(`Меня зовут ${MyName}`);
+
+ //  console.log(4 == 5 || 9 === 9);
+
+ // //  if (4 == 9) {
+ //      console.log('ok!');
+ //  } else {
+ //      console.log("eror");
+ //  }
+
+ //  const num = 50;
+ //  if (num < 49) {
+ //      console.log('Eror');
+ //  } else if (num > 100) {
+ //      console.log('mnogo');
+ //  } else {
+ //      console.log("ok");
+ //  }
+
+ //  (num == 50) ? console.log('Ok'): console.log('Eror');
+ //  const num = '50';
+ //  switch (num)   {
+ //      case '49':
+ //          console.log('Neverno');
+ //          break;
+ //      case '100':
+ //          console.log('Neverno');
+ //          break;
+ //      case '50':
+ //          console.log('true');
+ //          break;
+ //      default:
+ //          console.log('not now');
+ //          break;
+ //  }
+
+ let num = 50;
+ //  while (num <= 55) {
+ //      console.log(num);
+ //      num++;
+ //  }
+
+ //  do {
+ //      console.log(num);
+ //      num++;
+ //  }
+
+ //  while (num <= 55);
+
+ for (let i = 1; i < 10; i++) {
+     if (i == 6) {
+         //  break;
+         continue;
+     }
+     console.log(i);
+
+ }
